83fd910f9bfeddb5ac9244baaa350516dc033d00
Yukie <Yukie@mroindre.com>
2022-12-03 16:17:23 +0800

    Update so.sh

--- so.sh
+++ so.sh
@@ -1,7 +1,1 @@
-clang main.c Compile.c MultitThreadMemoryManager.c a/b/c.c MArrayList.c -c
-
-clang main.o Compile.o MultitThreadMemoryManager.o c.o MArrayList.o -lpthread -o demo
-
-cp demo /run/media/yukie/Yukie存储盘/SSLServer/RTMake
-
-cd /home/yukie/RTMake || ! echo "Failure" && ./RTMake
+clang main.o Compile.o MultitThreadMemoryManager.o c.o MArrayList.o -lpthread -o RTMake
